86e03f6a14fa56c5a3dc2e174345b61e419d0d4c
jesmage14 <jesmage14@gmail.com>
2014-09-03 19:35:38 -0600

    change

---
+++ src/main/java/tetramage/nuclearmayhem/tileentity/TileEntityWoodMill.java
@@ -0,0 +1,12 @@
+package tetramage.nuclearmayhem.tileentity;
+
+import net.minecraft.tileentity.TileEntity;
+
+public class TileEntityWoodMill extends TileEntity{
+
+	public void setGuiDisplayName(String displayName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
